e91db517c3f97bcd19487b1d4ab7ee8889ea73bb
fikreynurz <fikreynursyabani@gmail.com>
2024-10-13 21:46:58 +0700

    UPDATE: ubah home

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,32 +2,32 @@ import React, { useState, useEffect } from 'react';
 import { AppBar, Toolbar, Typography, Button, Menu, MenuItem, IconButton, Avatar, TextField, InputAdornment, Autocomplete } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import SearchIcon from '@mui/icons-material/Search';  // Tambahkan ikon pencarian
-import logo from '../../assets/images/logo.png';  // Pastikan path ini benar
-import FilterModal from '../Filter/FilterModal';  // Import modal filter
+import SearchIcon from '@mui/icons-material/Search';
+import logo from '../../assets/images/logo.png';
+import FilterModal from '../Filter/FilterModal';
 
 const Header = () => {
   const [anchorEl, setAnchorEl] = useState(null);
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Status login
-  const [role, setRole] = useState(''); // Role bisa 'user' atau 'admin'
-  const [name, setName] = useState(''); // Role bisa 'user' atau 'admin'
-  const [profilePic, setProfilePic] = useState(''); // Link ke foto profil
-  const [searchQuery, setSearchQuery] = useState('');  // State untuk search query
-  const [suggestions, setSuggestions] = useState([]);  // State untuk menyimpan saran pencarian
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [role, setRole] = useState('');
+  const [name, setName] = useState('');
+  const [profilePic, setProfilePic] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
   const navigate = useNavigate();
   const user = localStorage.getItem("user")
   const userParsed = JSON.parse(user)
 
   useEffect(() => {
     if (user) {
-      setIsLoggedIn(true)
-      setRole(userParsed.role)
-      setName(userParsed.name)
+      setIsLoggedIn(true);
+      setRole(userParsed.role);
+      setName(userParsed.name);
     }
-  })
+  }, [user]);
 
   const handleLogin = () => {
-    navigate('/login')  
+    navigate('/login');
   };
 
   const handleLogout = () => {
@@ -36,7 +36,7 @@ const Header = () => {
     setAnchorEl(null);
     setRole('');
     setProfilePic('');
-    navigate('/')
+    navigate('/');
   };
 
   const handleMenu = (event) => {
@@ -66,20 +66,18 @@ const Header = () => {
         const response = await axios.get(
           `https://api.themoviedb.org/3/search/multi?api_key=ac18a0e6818325589a5c34b35da509ab&language=en-US&query=${value}&page=1`
         );
-        console.log(response.data.results);
         setSuggestions(response.data.results);
       } catch (error) {
         console.error("Error fetching autocomplete suggestions:", error);
       }
     } else {
-      setSuggestions([]);  // Kosongkan suggestions jika input kosong
+      setSuggestions([]);
     }
   };
 
   return (
-    <AppBar position="sticky" sx={{ zIndex: 1100 }}>  {/* Menggunakan position sticky */}
+    <AppBar position="sticky" sx={{ zIndex: 1100 }}>
       <Toolbar>
-        {/* Logo dan judul JAKRIE */}
         <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
           <img src={logo} alt="Logo" style={{ width: '40px', height: '40px' }} />
         </IconButton>
@@ -92,48 +90,52 @@ const Header = () => {
           JAKRIE
         </Typography>
 
-        {/* Input Search dengan Autocomplete */}
-        <Autocomplete
-          freeSolo
-          options={suggestions.map((option) =>
-            option.title ? option.title : option.name  // Tampilkan title jika ada, jika tidak name
-          )}
-          onInputChange={handleInputChange}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Search Movies or Actors"
-              variant="outlined"
-              size="small"
-              value={searchQuery}
-              onKeyDown={handleKeyDown}  // Trigger pencarian ketika Enter ditekan
-              sx={{
-                backgroundColor: 'white',  // Latar belakang putih
-                borderRadius: '4px',  // Membuat ujung kotak search menjadi rounded
-                marginRight: '10px',
-                '& .MuiOutlinedInput-root': {
-                  '& fieldset': {
-                    borderColor: 'white',  // Hilangkan border default
-                  },
-                  '&:hover fieldset': {
-                    borderColor: '#1976d2',  // Warna border saat hover
-                  },
-                },
-              }}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <SearchIcon />
-                  </InputAdornment>
-                ),
-              }}
+        {/* Tampilkan search dan FilterModal jika role bukan admin */}
+        {role !== 'admin' && (
+          <>
+            {/* Input Search dengan Autocomplete */}
+            <Autocomplete
+              freeSolo
+              options={suggestions.map((option) =>
+                option.title ? option.title : option.name
+              )}
+              onInputChange={handleInputChange}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Search Movies or Actors"
+                  variant="outlined"
+                  size="small"
+                  value={searchQuery}
+                  onKeyDown={handleKeyDown}
+                  sx={{
+                    backgroundColor: 'white',
+                    borderRadius: '4px',
+                    marginRight: '10px',
+                    '& .MuiOutlinedInput-root': {
+                      '& fieldset': {
+                        borderColor: 'white',
+                      },
+                      '&:hover fieldset': {
+                        borderColor: '#1976d2',
+                      },
+                    },
+                  }}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <SearchIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              )}
             />
-          )}
-        />
 
-        <FilterModal />  {/* Modal Filter yang bisa di-trigger dari Header */}
+            <FilterModal />
+          </>
+        )}
 
-        {/* Login atau Profile setelah login */}
         {isLoggedIn ? (
           <>
             <IconButton onClick={handleMenu} color="inherit">
@@ -149,7 +151,6 @@ const Header = () => {
               <MenuItem onClick={handleClose}>Role: {role}</MenuItem>
               <MenuItem onClick={handleLogout}>Logout</MenuItem>
             </Menu>
-            {/* Profile button */}
             <Button color="inherit" component={Link} to="/profile">
               Profile
             </Button>
